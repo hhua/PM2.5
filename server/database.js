9936513a3ace57f1e8b78d9d284245aa7e707025
chinesecold <chinesecold@gmail.com>
2013-05-17 00:49:12 -0400

    heroku branch added

--- server/database.js
+++ server/database.js
@@ -1,144 +1,57 @@
 var mongo = require('mongodb');
 
-//======================================
-//      mongo init
-//          note -- unlike the notes, this deletes
-//                  everything in testCollection 
-//                  before calling openDb's callback
-//======================================
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/myDb'; 
 
-var host = 'localhost';
-var port = mongo.Connection.DEFAULT_PORT;
+function openDbInsert(doc){
+    mongo.Db.connect(mongoUri, function(err, db){
+        if(err)
+            throw err;
+        db.collection('cityCollection', onCityConnection);
 
-var optionsWithEnableWriteAccess = { w: 1 };
-var dbName = 'myDb';
+        function onCityConnection(er, collection){
+            if(er)
+                throw er;
+            collection.remove({}, function(error){
+                if(error)
+                    throw error;
+                insertDocument(collection, doc, onTestDocumentsInserted);
 
-var client = new mongo.Db(
-    dbName,
-    new mongo.Server(host, port),
-    optionsWithEnableWriteAccess
-);
+                function onTestDocumentsInserted(errr){
+                    if (errr)
+                      throw errr;
 
-//var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/myDb'; 
-
-function openDb(onOpen, doc){
-    client.open(onDbReady);
-
-    function onDbReady(error){
-        if (error)
-            throw error;
-        client.collection('cityCollection', onCityCollectionReady);
-    }
-
-    function onCityCollectionReady(error, cityCollection){
-        if (error)
-            throw error;
-        
-        cityCollection.remove({}, function(error){
-            if (error)
-                throw error;
-            onOpen(cityCollection, doc);
-        });
-    }
-}
-
-function closeDb(){
-    client.close();
+                    console.log('document inserted!');
+                    db.close();
+                }
+            });
+        }
+    });
 }
 
 function openDbQuery(query, response){
-    client.open(onDbReady);
-
-    function onDbReady(error){
-        if (error)
-            throw error;
-        client.collection('cityCollection', onCityCollectionReady);
-    }
-
-    function onCityCollectionReady(error, cityCollection){
-        if (error)
-            throw error;
-        
-        cityCollection.find(query).toArray(function(err, docs){
-            if(err)
-                throw err;
-
-            var parsedDocs = JSON.parse(JSON.stringify(docs));
-            console.log("docs " + JSON.stringify(parsedDocs[0].cities));
-            response.write(parsedDocs[0].cities);
-            console.log("Response was writen back to client!");
-            response.end();
-            closeDb();
-        });
-    }
-}
-
-//======================================
-//      inserting test documents
-//======================================
-
-//openDb(onDbOpen);
-
-function onDbOpen(collection, doc){
-    //var documents = [ { n: 1 }, { n: 2 }, { n: 3 }, { n: 4 } ];
-    //insertDocuments(collection, documents, onTestDocumentsInserted);
-    insertDocument(collection, doc, onTestDocumentsInserted);
-
-    function onTestDocumentsInserted(err){
-        if (err)
+    mongo.Db.connect(mongoUri, function(err, db){
+        if(err)
             throw err;
-        console.log('document inserted!');
-        //findDoc(collection);
-    }
-}
+        db.collection('cityCollection', onCityConnection);
 
-function insertDocuments(collection, docs, done){
-    if (docs.length === 0){
-        done(null);
-        return;
-    }
-    var docHead = docs.shift(); //shift removes first element from docs
-    collection.insert(docHead, function onInserted(err){
-        if (err){
-            done(err);
-            return;
+        function onCityConnection(er, collection){
+            if(er)
+                throw er;
+            
+            collection.find(query).toArray(function(error, docs){
+                if(error)
+                    throw error;
+
+                var parsedDocs = JSON.parse(JSON.stringify(docs));
+                console.log("docs " + JSON.stringify(parsedDocs[0].cities));
+                response.write(parsedDocs[0].cities);
+                console.log("Response was writen back to client!");
+                response.end();
+                db.close();
+            });
         }
-        insertDocuments(collection, docs, done);
     });
 }
 
-function insertDocument(collection, doc, done){
-    collection.insert(doc, function onInserted(err){
-        if(err){
-            done(err);
-            return;
-        }
-        done(null);
-        closeDb();
-    });
-}
-
-//======================================
-//      finding documents
-//======================================
-
-var logger = function(error, result){
-    if (error)
-        throw error;
-    console.log(result);
-    closeDb();
-}
-
-var logDoc = logger;
-var logDocs = logger;
-
-function findDoc(collection){
-    //var query = { n: { $gt: 2 } };
-    var query = {};
-    collection.find(query).toArray(logDocs);
-}
-
-exports.openDb = openDb;
+exports.openDbInsert = openDbInsert;
 exports.openDbQuery = openDbQuery;
-exports.onDbOpen = onDbOpen;
-exports.insertDocument = insertDocument;
--- worker.js
+++ worker.js
@@ -13,8 +13,8 @@ function init() {
 
 		// get AQI data
 		var http = require('http');
-		var url = 'http://pm25.in/api/querys/all_cities.json?token=' + api_key;
-		//var url = 'http://pm25.in/api/querys.json?token=' + api_key;
+		//var url = 'http://pm25.in/api/querys/all_cities.json?token=' + api_key;
+		var url = 'http://pm25.in/api/querys.json?token=' + api_key;
 
 		http.get(url, function(res) {
 			var body = '';
@@ -34,9 +34,9 @@ function init() {
 				// });
 
       			// write back to database
-      			var doc = { cities : body};
+      			var doc = { cities : body };
 
-      			database.openDb(database.onDbOpen, doc);
+      			database.openDbInsert(doc);
       		}).on('error', function(e){
       			console.log("Got error: " + e.message);
       		});
@@ -46,46 +46,4 @@ function init() {
 	}
 }
 
-setInterval(function() {
-	console.log("Get Latest AQI Information at " + (new Date()));
-
-	try{
-		// read API key
-		var fs = require('fs');
-		var data = fs.readFileSync('./metadata/API-KEY.txt');
-		var api_key = data.toString();
-
-		// get AQI data
-		var http = require('http');
-		var url = 'http://pm25.in/api/querys/all_cities.json?token=' + api_key;
-		//var url = 'http://pm25.in/api/querys.json?token=' + api_key;
-
-		http.get(url, function(res) {
-			var body = '';
-
-      		res.on('data', function(chunk) {
-         		body += chunk;
-      		});
-
-      		res.on('end', function() {
-        		var response = JSON.parse(JSON.stringify(body, null, 3));
-        		//console.log("Got response: " + response);
-
-        		// write file to json
-    //     		fs.writeFile('./metadata/allcities.json', body, function (err) {
-  		// 			if (err) throw err;
-  		// 			console.log('It\'s saved!');
-				// });
-
-      			// write back to database
-      			var doc = { cities : body};
-
-      			database.openDb(database.onDbOpen, doc);
-      		}).on('error', function(e){
-      			console.log("Got error: " + e.message);
-      		});
-		});
-	}catch(err){
-		throw err;
-	}
-}, 2700000);
+setInterval(init(), 2700000);
